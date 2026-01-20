d4ade2e2b0e0bfa089bf5e9db5ccf16d26bbbb0d
MidlandBarbs <adam.barbary@gmail.com>
2026-01-20 17:34:15 +1100

    ModDashV1 - EEZ Studio

---
+++ MogDashV1/src/ui/screens.h
@@ -0,0 +1,54 @@
+#ifndef EEZ_LVGL_UI_SCREENS_H
+#define EEZ_LVGL_UI_SCREENS_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _objects_t {
+    lv_obj_t *main;
+    lv_obj_t *my_panel_header;
+    lv_obj_t *my_label_panel_header;
+    lv_obj_t *front_left_container;
+    lv_obj_t *fl_portal_guage;
+    lv_obj_t *fl_portal_label;
+    lv_obj_t *front_left_label;
+    lv_obj_t *front_right_container;
+    lv_obj_t *fr_portal_guage;
+    lv_obj_t *fr_portal_label;
+    lv_obj_t *front_right_label;
+    lv_obj_t *rear_left_container;
+    lv_obj_t *rl_portal_guage;
+    lv_obj_t *rl_portal_label;
+    lv_obj_t *rear_left_label;
+    lv_obj_t *rear_right_container;
+    lv_obj_t *rr_portal_guage;
+    lv_obj_t *rr_portal_label;
+    lv_obj_t *rear_right_label;
+    lv_obj_t *alert_box;
+    lv_obj_t *my_horizontal_slider;
+    lv_obj_t *settings;
+} objects_t;
+
+extern objects_t objects;
+
+enum ScreensEnum {
+    SCREEN_ID_MAIN = 1,
+};
+
+void create_screen_main();
+void tick_screen_main();
+
+void tick_screen_by_id(enum ScreensEnum screenId);
+void tick_screen(int screen_index);
+
+void create_screens();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_SCREENS_H*/
