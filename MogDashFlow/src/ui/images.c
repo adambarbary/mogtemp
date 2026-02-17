1264fe814c2b08b45cc7d3454e5639e28fb4b706
MidlandBarbs <adam.barbary@gmail.com>
2026-02-17 21:43:06 +1100

    Add files via upload
    These EEZ  files are for the flow based versions. Go nuts and customise as you like.

---
+++ MogDashFlow/src/ui/images.c
@@ -0,0 +1,9 @@
+#include "images.h"
+
+const ext_img_desc_t images[5] = {
+    { "gradient", &img_gradient },
+    { "gear", &img_gear },
+    { "minidisk", &img_minidisk },
+    { "miniback", &img_miniback },
+    { "mog", &img_mog },
+};
